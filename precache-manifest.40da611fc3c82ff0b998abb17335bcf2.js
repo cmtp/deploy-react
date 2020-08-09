4653dfe741722b9705a5363c6253a01d220a1982
cmtp <christian.tola@hotmail.com>
2020-08-09 17:19:29 -0400

    Updates

---
+++ precache-manifest.40da611fc3c82ff0b998abb17335bcf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f9bacf279eba151b6959cca47b8d972",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb0c3092b7742174697c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a8eece0357e8605252d3",
+    "url": "/static/css/main.80308c82.chunk.css"
+  },
+  {
+    "revision": "fb0c3092b7742174697c",
+    "url": "/static/js/2.6150d6f0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6150d6f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8eece0357e8605252d3",
+    "url": "/static/js/main.c8ec70c9.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
